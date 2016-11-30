ad78bb2b7dc775eaad766522852348547a249dd8
Robert Bakker <r.m.bakker@live.nl>
2016-11-30 19:56:31 +0100

    Add threading

--- src/com/robertmark/Main.java
+++ src/com/robertmark/Main.java
@@ -13,11 +13,11 @@ public class Main {
 
     private Integer[] array = new Integer[SIZE];
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Main main = new Main();
         main.runOnSingleThread();
 
-        main.runOnMultipleThreads();
+        main.runOnTwoThreads();
     }
 
     public void runOnSingleThread() {
@@ -50,10 +50,35 @@ public class Main {
         printNElements(10);
     }
 
-    public void runOnMultipleThreads() {
-        System.out.println("\nMerge sort on multiple cores next.");
-        System.out.println(String.format("Available cores: %d\n", THREADS));
+    public void runOnTwoThreads() throws InterruptedException {
+//        System.out.println("\nMerge sort on multiple cores next.");
+//        System.out.println(String.format("Available cores: %d\n", THREADS));
 
+        int[] array = new int[SIZE];
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = random.nextInt(SIZE);
+        }
+        System.out.println("Filling array with random values...\n");
+        printNElements(10);
+
+        stopWatch.reset();
+        stopWatch.start();
+        int[] subArr1 = new int[array.length / 2];
+        int[] subArr2 = new int[array.length - array.length / 2];
+        System.arraycopy(array, 0, subArr1, 0, array.length / 2);
+        System.arraycopy(array, array.length / 2, subArr2, 0, array.length - array.length / 2);
+
+        MergeSortThread runner1 = new MergeSortThread(subArr1);
+        MergeSortThread runner2 = new MergeSortThread(subArr2);
+        runner1.start();
+        runner2.start();
+        runner1.join();
+        runner2.join();
+        finalMerge(runner1.getInternal(), runner2.getInternal());
+        stopWatch.stop();
+        System.out.println("2-thread MergeSort takes: " + stopWatch.getTime() + " ms");
+        printNElements(10);
     }
 
     private void printNElements(int n) {
@@ -62,5 +87,37 @@ public class Main {
             System.out.print(array[i] + ",");
         }
         System.out.println();
+    }
+
+    public static void finalMerge(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int r = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j]) {
+                result[r] = a[i];
+                i++;
+                r++;
+            } else {
+                result[r] = b[j];
+                j++;
+                r++;
+            }
+            if (i == a.length) {
+                while (j < b.length) {
+                    result[r] = b[j];
+                    r++;
+                    j++;
+                }
+            }
+            if (j == b.length) {
+                while (i < a.length) {
+                    result[r] = a[i];
+                    r++;
+                    i++;
+                }
+            }
+        }
     }
 }
