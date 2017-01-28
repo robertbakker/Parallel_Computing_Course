90240f29e06af7b1c860e1c6cb7334b4b573b8ba
Robert Bakker <r.m.bakker@live.nl>
2017-01-28 22:23:12 +0100

    Optimize parallel clojure

--- src/com/robertmark/image3.clj
+++ src/com/robertmark/image3.clj
@@ -14,113 +14,81 @@
      (prn (str ~printStr " - Elapsed time: " (/ (double (- (. System (nanoTime)) start#)) 1000000.0) " msecs"))
      ret#))
 
-(defn basePath []
+(def n-threads (.availableProcessors (Runtime/getRuntime)))
+
+(defn base-path []
   (.getAbsolutePath (new File ""))
   )
 
 (defn path [p]
-  (clojure.string/join "/../../../" [(basePath), p]))
+  (clojure.string/join "/../../../" [(base-path), p]))
 
-(defn readFile
-  [path]
-  (new File path))
-
-(defn setpxl [^BufferedImage image data]
+(defn gray-pixel
+  [^BufferedImage bufferedImage, x, y]
   (let [
-        h (.getHeight image)
-        w (.getWidth image)
-        ]
-    (.setRGB image 0 0 w h data 0 w)
-    )
-  )
-
-(defn getrgb [rgb]
-  (let [r (bit-shift-right (bit-and rgb (int 0x00FF0000)) 16)
-        g (bit-shift-right (bit-and rgb (int 0x0000FF00)) 8)
-        b (bit-and rgb (int 0x000000FF))]
-    [r g b])
-  )
-
-(defn grayPixel
-  [rgb]
-  (let [
+        rgb (.getRGB bufferedImage x y),
         r (bit-and (bit-shift-right rgb 16) 0xFF),
         g (bit-and (bit-shift-right rgb 8) 0xFF),
         b (bit-and rgb 0xFF),
         grayLevel (int (/ (+ r g b) 3)),
+        gray (+ (bit-shift-left grayLevel 16) (bit-shift-left grayLevel 8) grayLevel)
         ]
-    (+ (bit-shift-left grayLevel 16) (bit-shift-left grayLevel 8) grayLevel)
+    (.setRGB bufferedImage x y gray)
     )
   )
 
+(defn read-buffered-image [fileName]
+  (javax.imageio.ImageIO/read (as-file (path fileName))))
 
-(defn getpxl [^BufferedImage img]
-  (.getRGB img 0 0 (.getWidth img) (.getHeight img) nil 0 (.getWidth img))
-  )
-
-(defn makeItGray [pixels]
-  (time-print "making it gray" (doall (map grayPixel pixels)))
-  )
-(defn graycalc [[r g b]]
-  (let [gray (int (/ (+ r g b) 3))
-        r (bit-shift-left gray 16)
-        g (bit-shift-left gray 8)
-        b gray
-        a (bit-shift-left 0x00 24)
-        ]
-    (int (bit-or a r g b))))
-
-(defn grayThread [^BufferedImage, pixels]
-
-  )
-
-(defn getPixels [^BufferedImage img, startX]
-  (.getRGB img startX 0 (.getWidth img) (.getHeight img) nil 0 (.getWidth img))
-  )
-
-(defn readImage [fileName]
-  (javax.imageio.ImageIO/read (as-file (path fileName)))
-  )
-
-
-(defn grayThreaded [fileName, threads]
-  (let [
-        img (time-print "Reading image" (readImage fileName))
-        h (.getHeight img)
-        w (.getWidth img)
-        widthChunk (* h (Math/ceil (/ w threads)))
-        pixels (time-print "Get pixels" (getpxl img))
-        partitionedPixels (time-print "Partitioning the data" (partition-all widthChunk pixels))
-        gray (pmap makeItGray partitionedPixels)
-        grayResult (time-print "Calculating result in parallel" (doall (apply concat gray)))
-        ]
-    (time-print "Setting pixel data back into buffered image" (setpxl img (int-array grayResult)))
-    (javax.imageio.ImageIO/write
-      img "jpg" (new File "test.jpg")
-      )))
-
-(defn testrgb []
-  (let [img (time (javax.imageio.ImageIO/read (as-file (path "vissenkom.jpg"))))
-        h (.getHeight img)
-        w (.getWidth img)
-        arr (time (int-array (getpxl img)))
-        gray (time
-               ;;why is amap so slow?
-
-               ;;400ms
-               ;(int-array (map #(graycalc (getrgb %1)) arr))
-
-
-               ;;8000ms
-               (amap ^ints arr idx ret ^int (graycalc (getrgb (aget ^ints arr idx))))
-               )
-        ]
-    (time (setpxl img gray))
-    (javax.imageio.ImageIO/write
-      img "jpg" (new File "test.jpg")
-      )
-
+(defn save-buffered-image [^BufferedImage img, name]
+  (javax.imageio.ImageIO/write
+    img "jpg" (new File (path name))
     ))
 
-;(testrgb)
-(time-print "Full process" (grayThreaded "image.jpg" 4))
+(defn gray-serial [fileName, outputFileName]
+  (let [
+        img ^BufferedImage (read-buffered-image fileName)
+        h (.getHeight img)
+        w (.getWidth img)
+        width-range (range w)
+        height-range (range h)
+        ]
+    (doseq [x width-range y height-range]
+      (gray-pixel ^BufferedImage img x y)
+      )
+    (save-buffered-image img outputFileName)
+    )
+  )
+
+(defn gray-parallel [fileName, outputFileName]
+  (let [
+        img ^BufferedImage (read-buffered-image fileName)
+        h (.getHeight img)
+        w (.getWidth img)
+        partition-size (Math/ceil (/ w n-threads))
+        width-range (range w)
+        height-range (range h)
+        partitions (partition-all partition-size width-range)
+        ]
+    (doall
+      (pmap
+        (fn [partition]
+          (doseq [x partition y height-range]
+            (gray-pixel ^BufferedImage img x y)
+            ))
+        partitions)
+      )
+    (save-buffered-image img outputFileName)
+    )
+  )
+
+(println "Using " n-threads " threads.")
+(println "")
+(time-print "Small - Single Threaded" (gray-serial "vissenkom.jpg" "gray_clojure_vissenkom_serial.jpg"))
+(time-print "Small - Multi-Threaded" (gray-parallel "vissenkom.jpg" "gray_clojure_vissenkom_parallel.jpg"))
+(println "")
+(time-print "Medium - Single Threaded" (gray-serial "hond.jpg" "gray_clojure_hond_serial.jpg"))
+(time-print "Medium - Multi-Threaded" (gray-parallel "hond.jpg" "gray_clojure_hond_parallel.jpg"))
+(println "")
+(time-print "Large - Single Threaded" (gray-serial "image.jpg" "gray_clojure_image_serial.jpg"))
+(time-print "Large - Multi-Threaded" (gray-parallel "image.jpg" "gray_clojure_image_parallel.jpg"))
